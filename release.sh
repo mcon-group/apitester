7071a2836cdccc2768b72a28993ccf3dbd3b4f12
rm <ruben.malchow@mcon-group.com>
2018-04-13 20:37:57 +0200

    fixed release script

--- release.sh
+++ release.sh
@@ -18,7 +18,7 @@ project_dir=`realpath $0`
 local_maven=${HOME}/maven-mcon
 
 if [ ! -d ${local_maven} ]; then
-	mkdirs -p ${local_maven}
+	mkdir -p ${local_maven}
 	git clone git@github.com:mcon-group/maven.git ${local_maven}
 fi
 
