648d14a7d83c2d4b12bf11f1d51ca6a29a410730
Ruben Malchow <ruben.malchow@mcon-group.com>
2020-03-12 16:57:04 +0100

    fix redirect

--- apitester-ui/src/main/java/com/mcg/apitester/ui/controllers/ApitesterIndexController.java
+++ apitester-ui/src/main/java/com/mcg/apitester/ui/controllers/ApitesterIndexController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.mcg.apitester.api.annotations.ApiIgnore;
 
 @RestController
@@ -19,6 +21,11 @@ public class ApitesterIndexController {
 	@RequestMapping(value="/apitester",method=RequestMethod.GET)
 	public String get(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+		try {
+			System.err.println(" === TUTTI ===> "+ new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT).writeValueAsString(request));
+		} catch (Exception e) {
+			
+		}
 		String prefix = "";
 		{
 			Enumeration<String> x = request.getHeaders("x-forwarded-prefix");
@@ -28,17 +35,19 @@ public class ApitesterIndexController {
 		String host = "";
 		{
 			Enumeration<String> x = request.getHeaders("x-forwarded-host");
-			if(x.hasMoreElements()) host = x.nextElement();
+			if(x.hasMoreElements()) host = x.nextElement().split(",")[0];
 		}
 		
 		String proto = "https";
 		{
 			Enumeration<String> x = request.getHeaders("x-forwarded-proto");
-			if(x.hasMoreElements()) proto = x.nextElement();
+			if(x.hasMoreElements()) proto = x.nextElement().split(",")[0];
 		}
 
+		
+		
 		response.setStatus(302);
-		response.setHeader("Location", proto+host+prefix+"/apitester/index.html");
+		response.setHeader("Location", proto+"://"+host+prefix+"/apitester/index.html");
 		return null;
 	}
 	
---
+++ apitester-ui/src/main/java/com/mcg/apitester/ui/controllers/WebConfig.java
@@ -0,0 +1,15 @@
+package com.mcg.apitester.ui.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	    registry.addViewController("/apitester").setViewName("forward:/apitester/index.html");
+	}	
+	
+}
