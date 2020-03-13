1b1acbeec8ef5112f5f328fe8d659fafaae1487d
ruben <ruben.malchow@gmail.com>
2020-03-13 09:44:12 +0100

    fix web config naming

--- apitester-ui/src/main/java/com/mcg/apitester/ui/controllers/ApitesterWebConfig.java
+++ apitester-ui/src/main/java/com/mcg/apitester/ui/controllers/ApitesterWebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class ApitesterWebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
